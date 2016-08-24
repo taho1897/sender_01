306e6434759cea6060e8a979c2c9ac1b32e5d10d
Tacademy <@>
2016-08-24 17:34:03 +0900

    2

--- routes/chatting.js
+++ routes/chatting.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
+var path = require('path');
+var url = require('url');
+var isSecure = require('./common').isSecure;
+var isAuthenticated = require('./common').isAuthenticated;
+var ecTo = 'http://ec2-52-78-70-38.ap-northeast-2.compute.amazonaws.com:3000';
 
-router.post('/', function(req, res, next) {
+router.post('/', isSecure, isAuthenticated, function(req, res, next) {
+    var form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.multiples = true;
+    form.uploadDir = path.join(__dirname, '../uploads/images/menus');
+    form.parse(req, function(err, fields, files) {
+        if (err) { return next(err);}
+        var data = {};
+        data.receiverId = fields.receiver_id;
+        data.name = fields.name;
+        data.message = fields.message;
+        data.pic = [];
+        data.pic.push(files.pic);
 
+        var filename = path.basename(files.pic.path);
+        data.pic.push({url : url.resolve(ecTo,'/images/'+filename)});
+
+        res.send({
+            result : data
+        });
+
+    });
 });
 
 module.exports = router;
