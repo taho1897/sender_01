d21da1d6c87e1cae027f71a2b247b46b0f07f9ae
Tacademy <@>
2016-08-24 10:46:50 +0900

    2 - 7 완료 ( 5 제와 _ 사진은 왜 업로드가 안되는가)

--- routes/member.js
+++ routes/member.js
@@ -1,21 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
+var path = require('path');
+var async = require('async');
+var url = require('url');
 
 router.put('/', function(req, res, next) {
     var phone = req.body.phone;
     if (phone !== undefined) {
-        req.send({
+        res.send({
             message: phone + ': dummy 사용자 등록을 성공했습니다.'
         });
     } else {
-        req.send({
+        res.send({
             message: 'dummy 사용자 등록을 실패했습니다.'
         });
     }
 }); // 2. 핸드폰 번호 등록
 
 router.get('/me', function(req, res, next) {
-    req.send({
+    res.send({
         result: {
             user_id : 1,
             name : '홍길동',
@@ -30,11 +34,16 @@ router.get('/me', function(req, res, next) {
     });
 }); // 3. 자신의 정보 보기
 
+router.get('/logout', function(req, res, next) {
+    req.logout();
+    res.send({ message : '로그아웃했습니다.' });
+}); // 6. 로그아웃
+
 router.get('/:user_id', function(req, res, next) {
     var userId = req.params.user_id;
-    req.send({
+    res.send({
         result : {
-            user_id: 2,
+            user_id: userId,
             name: '홍길동생',
             email: 'hongsang@hwalbin.com',
             phone: '010-1111-1111',
@@ -55,26 +64,34 @@ router.put('/me', function(req, res, next) {
         if (err) {return next(err);}
         var menu = {};
         menu.files = [];
-        if (files.photos) {
             menu.files.push(files.photos);
+            var filename = path.basename(files.photos.path);
+            menu.files.push({url : url.resolve('http://localhost:3000','/images/'+filename)});
             form.uploadDir = path.join(__dirname, '../uploads/images/menus');
             res.send({
-                message: '프로필 사진의 변경을 성공하였습니다.'
+                message: '프로필 사진의 변경을 성공하였습니다.',
+                result : menu
+            });
+    });
+}); // 5. 자신의 프로필 사진 변경 하기
+
+/*        if (files.photos) {
+            menu.files.push(files.photos);
+            var filename = path.basename(files.photos.path);
+            menu.files.push({url : url.resolve('http://localhost:3000','/images/'+filename)});
+            res.send({
+                message : '프로필 사진의 변경을 성공하였습니다.',
+                result : menu
             });
         } else {
             res.send({
                 error : '프로필 사진의 변경을 실패했습니다.'
             });
-        }
-    });
-}); // 5. 자신의 프로필 사진 변경 하기
-
-router.get('/logout', function(req, res, next) {
-
-}); // 6. 로그아웃
+        }*/
 
 router.delete('/', function(req, res, next) {
-
+    var userId = req.body.user_id;
+    res.send({ message: userId +' : 회원 탈퇴가 처리되었습니다.' });
 }); // 7. 회원 탈퇴 하기
 
 module.exports = router;
